2b7feb8c0e0c2da468afd4a74aa62931be3eab32
inveders <alexandra.gnimadi@gmail.com>
2019-07-01 16:27:56 +0200

    Deleting xml files for Connexion Activity. Configuring MainActivity, ProfileActivity and BaseActivity.

--- app/src/main/java/com/inved/go4lunch/auth/ProfileActivity.java
+++ app/src/main/java/com/inved/go4lunch/auth/ProfileActivity.java
@@ -1,8 +1,141 @@
 package com.inved.go4lunch.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import androidx.appcompat.app.AlertDialog;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.inved.go4lunch.R;
+import com.inved.go4lunch.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class ProfileActivity extends BaseActivity {
+
+    //FOR DESIGN
+    @BindView(R.id.profile_activity_imageview_profile)
+    ImageView imageViewProfile;
+    @BindView(R.id.profile_activity_edit_text_username)
+    TextInputEditText textInputEditTextUsername;
+    @BindView(R.id.profile_activity_text_view_email)
+    TextView textViewEmail;
+    @BindView(R.id.profile_activity_progress_bar)
+    ProgressBar progressBar;
 
 
+    //FOR DATA
+    // 2 - Identify each Http Request
+    private static final int SIGN_OUT_TASK = 10;
+    private static final int DELETE_USER_TASK = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.configureToolbar();
+        this.updateUIWhenCreating(); //
+    }
+
+    @Override
+    public int getFragmentLayout() { return R.layout.activity_profile; }
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.profile_activity_button_update)
+    public void onClickUpdateButton() { }
+
+    @OnClick(R.id.profile_activity_button_sign_out)
+    public void onClickSignOutButton() {
+        this.signOutUserFromFirebase();
+        }
+
+    @OnClick(R.id.profile_activity_button_delete)
+    public void onClickDeleteButton() {
+            new AlertDialog.Builder(this)
+                    .setMessage(R.string.popup_message_confirmation_delete_account)
+                    .setPositiveButton(R.string.popup_message_choice_yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            deleteUserFromFirebase();
+                        }
+                    })
+                    .setNegativeButton(R.string.popup_message_choice_no, null)
+                    .show();
+        }
+
+        // --------------------
+        // REST REQUESTS
+        // --------------------
+        // Create http requests (SignOut & Delete)
+
+        private void signOutUserFromFirebase(){
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted(SIGN_OUT_TASK));
+        }
+
+        private void deleteUserFromFirebase(){
+            if (this.getCurrentUser() != null) {
+                AuthUI.getInstance()
+                        .delete(this)
+                        .addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted(DELETE_USER_TASK));
+            }
+        }
+
+        // --------------------
+        // UI
+        // --------------------
+
+    // Update UI when activity is creating
+    private void updateUIWhenCreating(){
+
+        if (this.getCurrentUser() != null){
+
+            //Get picture URL from Firebase
+            if (this.getCurrentUser().getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(this.getCurrentUser().getPhotoUrl())
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(imageViewProfile);
+            }
+
+            //Get email & username from Firebase
+            String email = TextUtils.isEmpty(this.getCurrentUser().getEmail()) ? getString(R.string.info_no_email_found) : this.getCurrentUser().getEmail();
+            String username = TextUtils.isEmpty(this.getCurrentUser().getDisplayName()) ? getString(R.string.info_no_username_found) : this.getCurrentUser().getDisplayName();
+
+            //Update views with data
+            this.textInputEditTextUsername.setText(username);
+            this.textViewEmail.setText(email);
+        }
+    }
+
+        // Create OnCompleteListener called after tasks ended
+        private OnSuccessListener<Void> updateUIAfterRESTRequestsCompleted(final int origin){
+            return new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    switch (origin){
+                        case SIGN_OUT_TASK:
+                            finish();
+                            break;
+                        case DELETE_USER_TASK:
+                            finish();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            };
+        }
 }
--- app/src/main/java/com/inved/go4lunch/controller/MainActivity.java
+++ app/src/main/java/com/inved/go4lunch/controller/MainActivity.java
@@ -2,15 +2,194 @@ package com.inved.go4lunch.controller;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+import com.inved.go4lunch.R;
+
+import com.inved.go4lunch.api.UserHelper;
+import com.inved.go4lunch.auth.ProfileActivity;
+import com.inved.go4lunch.base.BaseActivity;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    //FOR DESIGN
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.login_facebook_button)
+    Button facebookLogin;
+    @BindView(R.id.login_google_button)
+    Button googleLogin;
+
+    //FOR DATA
+    private static final int RC_SIGN_IN = 123;
+
+
+    @Override
+    public int getFragmentLayout() {
+        Log.d("DEBAGO", "MainActivity : getfragmentlayout ");
+        return R.layout.activity_main; }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     //   this.setContentView(this.getFragmentLayout());
+
+
+        Log.d("DEBAGO", "MainActivity : onCreate ");
+
+        // Start appropriate activity
+        if (this.isCurrentUserLogged()){
+            this.startProfileActivity();
+            }
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.updateUIWhenGoogleResuming();
+    }
+
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.login_facebook_button)
+    public void onClickFacebookLoginButton() {
+        if (this.isCurrentUserLogged()){
+            this.startProfileActivity();
+        } else {
+            this.startFacebookSignInActivity();
+        }
+    }
+
+    @OnClick(R.id.login_google_button)
+    public void onClickGoogleLoginButton() {
+        if (this.isCurrentUserLogged()){
+            this.startProfileActivity();
+        } else {
+            this.startGoogleSignInActivity();
+        }
+    }
+
+    // --------------------
+    // REST REQUEST
+    // --------------------
+
+    private void createUserInFirestore(){
+
+        if (this.getCurrentUser() != null){
+
+            String urlPicture = (this.getCurrentUser().getPhotoUrl() != null) ? this.getCurrentUser().getPhotoUrl().toString() : null;
+            String username = this.getCurrentUser().getDisplayName();
+            String uid = this.getCurrentUser().getUid();
+
+           // UserHelper.createUser(uid, username, urlPicture).addOnFailureListener(this.onFailureListener());
+        }
+    }
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    private void startFacebookSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.FacebookBuilder().build())) // FACEBOOK
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_appli)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void startGoogleSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build()))//GOOGLE
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_appli)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    // Launch Profile Activity
+    private void startProfileActivity(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+    // --------------------
+    // UI
+    // --------------------
+
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // Update UI when activity is resuming
+    private void updateUIWhenGoogleResuming(){
+        //this.buttonLogin.setText(this.isCurrentUserLogged() ? getString(R.string.button_login_text_logged) : getString(R.string.button_login_text_not_logged));
+    }
+
+    private void updateUIWhenFacebookResuming(){
+        this.facebookLogin.setText(this.isCurrentUserLogged() ? getString(R.string.button_login_text_logged) : getString(R.string.button_login_text_not_logged));
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                this.createUserInFirestore();
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+
+
+
 }
