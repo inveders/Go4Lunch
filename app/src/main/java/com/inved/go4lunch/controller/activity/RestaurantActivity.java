192b52619fb2f0eb8059948330d2ae17a3d93e3a
inveders <alexandra.gnimadi@gmail.com>
2019-10-14 07:46:13 +0200

    Re-Implementation of location listener and location manager to have the current geolocalisation.

--- app/src/main/java/com/inved/go4lunch/controller/activity/RestaurantActivity.java
+++ app/src/main/java/com/inved/go4lunch/controller/activity/RestaurantActivity.java
@@ -1,13 +1,18 @@
 package com.inved.go4lunch.controller.activity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -21,6 +26,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.viewpager.widget.ViewPager;
 
@@ -50,9 +56,12 @@ import com.inved.go4lunch.controller.fragment.MapFragment;
 import com.inved.go4lunch.controller.fragment.PeopleFragment;
 import com.inved.go4lunch.firebase.User;
 import com.inved.go4lunch.firebase.UserHelper;
+import com.inved.go4lunch.model.ResultModel;
 import com.inved.go4lunch.notification.NotificationsActivity;
 import com.inved.go4lunch.repository.NearbyRestaurantsRepository;
+import com.inved.go4lunch.utils.ManageAppMode;
 import com.inved.go4lunch.utils.ManageAutocompleteResponse;
+import com.inved.go4lunch.utils.ManageJobPlaceId;
 
 import java.util.Arrays;
 import java.util.List;
@@ -88,10 +97,11 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
     //FOR LOCATION
     protected PlacesClient placesClient;
     Location location; // location
+    private LocationManager lm;
     // The minimum distance to change Updates in meters
-  //  private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1000; // 1000 meters for tests, after come back to 10 meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1000; // 1000 meters for tests, after come back to 10 meters
     // The minimum time between updates in milliseconds
-   // private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute*/
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute*/
 
     //AUTOCOMPLETE
     AutocompleteSessionToken token;
@@ -102,7 +112,7 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
     double longitude; // longitude
 
     //FOR DATA
-
+    private ResultModel resultModel;
     MenuItem logout;
     TextView navFirstname;
     TextView navLastname;
@@ -191,7 +201,7 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
         this.configureDrawerLayout();
         this.configureNavigationView();
 
-      //  this.checkLocation();
+        this.checkLocation();
         // Initialize Places.
         if (!Places.isInitialized()) {
             Places.initialize(getApplicationContext(), getString(R.string.google_api_key));
@@ -208,8 +218,32 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
 
         //Localisation
 
+        //Check data
+        //Assign the value to declared resultModel variable
+        resultModel = ViewModelProviders.of(this).get(ResultModel.class);
 
 
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void checkLocation() {
+
+        //Subscribe to providers
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+        }
+
+        if (lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
+            lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+        }
+
+        if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+        }
     }
 
     @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION})
@@ -509,7 +543,7 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
     @Override
     public void onLocationChanged(Location location) {
 
-        Log.e("debago","onlocation changed");
+        //Log.e("debago","onlocation changed");
         latitude = location.getLatitude();
         longitude = location.getLongitude();
         sendLocationDataToFragments();
@@ -526,6 +560,8 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
         intent.putExtra(KEY_LATITUDE, getLatitude());
         intent.putExtra(KEY_LONGITUDE, getLongitude());
 
+
+        this.checkDistanceFromWork(currentGeolocalisation, ManageJobPlaceId.getJobPlaceId(this,ManageJobPlaceId.KEY_JOB_PLACE_ID));
         LocalBroadcastManager.getInstance(RestaurantActivity.this).sendBroadcast(intent);
 
     }
@@ -678,6 +714,62 @@ public class RestaurantActivity extends BaseActivity implements NavigationView.O
                 .setCancelable(false)
                 .setMessage(R.string.permission_acces_location_fine_rationale)
                 .show();
+    }
+
+
+    //HANDLING MODE
+
+    //Handle two modes for user : work mode and normal mode to be located everywhere
+    private void checkDistanceFromWork(String origins,String destinations){
+
+        resultModel.getMatrixDistance(origins,destinations).observe(this, result -> {
+
+            String appMode = ManageAppMode.getAppMode(this);
+            int distance = (result.get(0).getElements().get(0).getDistance().getValue())/1000;
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            // Add the buttons
+            builder.setPositiveButton(R.string.alert_dialog_location_far_from_works_pos_button, (dialog, which) -> {
+                // do something like...
+                changeMode(false);
+                Log.d("debago","end to click ok");
+            });
+            builder.setNegativeButton(R.string.alert_dialog_location_far_from_works_neg_button, (dialog, which) -> {
+                // do something like...
+                changeMode(true);
+                Log.d("debago","end to click no");
+            });
+            builder.setMessage(R.string.alert_dialog_location_far_from_works_text);
+
+            // Create the AlertDialog
+            AlertDialog dialog = builder.create();
+
+            if(distance>2 && appMode.equals("normal")){
+
+                if(dialog.isShowing()){
+                    Log.d("debago","dialog is not showed :");
+
+                }else{
+                    Log.d("debago","dialog is already showed :");
+                    dialog.show();
+                }
+
+            }
+            else if(distance<2 && appMode.equals("normal")){
+                changeMode(true);
+
+            }
+
+        });
+
+    }
+
+    private void changeMode(boolean workModeDesired) {
+        if(workModeDesired){
+            ManageAppMode.saveAppMode(this,"work");
+        }else{
+            ManageAppMode.saveAppMode(this,"normal");
+        }
     }
 
 
