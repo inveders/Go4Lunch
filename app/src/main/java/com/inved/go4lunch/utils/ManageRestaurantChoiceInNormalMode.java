35d6bb226dce7934329ea3f311c0069d5ebcd569
inveders <alexandra.gnimadi@gmail.com>
2019-10-16 19:07:52 +0200

    Create shared preferences class management to manage restaurant choice in normal mode

---
+++ app/src/main/java/com/inved/go4lunch/utils/ManageRestaurantChoiceInNormalMode.java
@@ -0,0 +1,23 @@
+package com.inved.go4lunch.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class ManageRestaurantChoiceInNormalMode {
+
+    private static final String KEY_RESTAURANT_CHOICE = "KEY_RESTAURANT_CHOICE";
+    private static final String KEY_RESTAURANT_CHOICE_DATA = "KEY_RESTAURANT_CHOICE_DATA";
+
+    public static void saveRestaurantChoice(Context context, String restaurantChoice) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_RESTAURANT_CHOICE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_RESTAURANT_CHOICE_DATA, restaurantChoice);
+        editor.apply();
+    }
+
+    public static String getRestaurantChoice(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_RESTAURANT_CHOICE, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_RESTAURANT_CHOICE_DATA, "work");
+    }
+
+}
